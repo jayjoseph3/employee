ac3b9a7626ef0f7e8b18766f0cfb3cdbbaf30ba6
unknown <johanesjoseph@yahoo.com.sg>
2019-11-11 00:12:21 +0800

    first commit

---
+++ EmployeeService/Models/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService.Models;
+
+namespace EmployeeService.Models
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
